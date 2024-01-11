ec7ea12c63a7c2f692e878b816c67788f9d1cb88
ADMIN <ADMIN@DESKTOP-IEDP0B2>
2024-01-11 11:33:57 +0530

    by harsh

--- Git_Prg/src/com/cjc/test/test.java
+++ Git_Prg/src/com/cjc/test/test.java
@@ -1,10 +1,36 @@
 package com.cjc.test;
 
-public class test {
-
+public class test 
+{
+	public void A()
+	{
+		System.out.println("A");
+	}
+	
+	public void B()
+	{
+		System.out.println("B");
+	}
+	
+	public void C()
+	{
+		System.out.println("C");
+	}
+	
+	public void D()
+	{
+		System.out.println("D");
+	}
+	
+	public void E()
+	{
+		System.out.println("E");
+	}
+	
+	
 	public static void main(String[] args) 
 	{
-		System.out.println("h1");
+		System.out.println("Exit");
 		
 
 	}
